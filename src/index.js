7a7e7b3146f07b009c9d28b1f43a4ac073b7b4cd
Yuri Pachkovsky <yuri.pachkovsky@synesis.ru>
2020-09-30 20:19:28 +0300

    feat: Add all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array)
+    return array.reduce( (p, v) => p < v ? p : v , 0);
+  return 0
 }
 
 exports.max = function max (array) {
+  if (array)
+    return array.reduce( (p, v) => p > v ? p : v , 0);
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array){
+    if (array.length != 0)
+      return array.reduce( (p, v) => p + v , 0)/array.length;
+    }
   return 0;
 }
